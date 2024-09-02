54e5a371affcf2d2bafd0bcd104cd274c3b965ea
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2024-09-02 11:51:17 +0300

    Some extra changes, still not done though.

--- repo_scan.py
+++ repo_scan.py
@@ -6,10 +6,15 @@ from bs4 import BeautifulSoup
 def get_repos_for_scan():
     repo_url_list = []
 
-    with open("repos_info.json", "r") as repo_info_json:
-        pass
+    with open("repos_info.json", "r") as file:
+        repos_info_json = json.load(file)
+    
+    for repo in repos_info_json:
+        repo_url_list.append(repo["url"])
 
     return repo_url_list
+
+
 def check_repo_terms():
     pass
 
@@ -18,3 +23,11 @@ def look_for_new_terms():
 
 def full_scan():
     pass
+
+
+def dev_test():   
+    for repo_url in get_repos_for_scan():
+        print(repo_url)
+
+dev_test()
+
