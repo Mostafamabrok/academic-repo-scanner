a3ec13dac29dd5915a745785b9849a2261379987
Mostafamabrok <mostafa.m.mabrok@gmail.com>
2024-09-02 11:16:06 +0300

    Setting up a bit of code, not working right now though.

--- repo_scan.py
+++ repo_scan.py
@@ -4,8 +4,12 @@ import json
 from bs4 import BeautifulSoup
 
 def get_repos_for_scan():
-    pass
+    repo_url_list = []
 
+    with open("repos_info.json", "r") as repo_info_json:
+        pass
+
+    return repo_url_list
 def check_repo_terms():
     pass
 
